c81a49b0e2211e826d1afaa9691b72e0875b1ca3
edikiuspy <u96_edumun_waw@technischools.com>
2024-03-01 10:35:55 +0100

    fix integration test

--- tests/integrations_tests.py
+++ tests/integrations_tests.py
@@ -1,30 +1,32 @@
 import pytest
-from po2.src.structure import User, Group
-from po2.src.logic import Logic
+from po2.src.api import app
 
 @pytest.fixture
-def logic():
-    return Logic()
+def client():
+    with app.test_client() as client:
+        yield client
 
-def test_integration_add_and_get_user(logic):
-    user = {"firstName": "Eduard", "lastName": "Muntianov", "birthYear": 2007, "group": Group("user")}
-    logic.add_user(user)
-    added_user = logic.get_user(1)
-    expected_user = {"id": 1, "firstName": "Eduard", "lastName": "Muntianov", "birthYear": 2007, "group": Group("user")}
-    assert added_user == expected_user
-
-def test_integration_update_and_get_user(logic):
+def test_integration_add_and_get_user(client):
     user = {"firstName": "Eduard", "lastName": "Muntianov", "birthYear": 2007, "group": "user"}
-    logic.add_user(user)
-    updated_user = {"firstName": "Anna", "lastName": "Muntianova", "birthYear": 2007, "group": Group("user")}
-    logic.update_user(1, updated_user)
-    updated_user = logic.get_user(1)
-    expected_user = {"id": 1, "firstName": "Anna", "lastName": "Muntianova", "birthYear": 2007, "group": Group("user")}
-    assert updated_user == expected_user
+    response = client.post('/users', json=user)
+    assert response.status_code == 201
+    response = client.get('/users/1')
+    assert response.status_code == 200
+    assert response.get_json() == {"id": 1, "firstName": "Eduard", "lastName": "Muntianov", "birthYear": 2007, "group": "user"}
 
-def test_integration_delete_user(logic):
+def test_integration_update_and_get_user(client):
     user = {"firstName": "Eduard", "lastName": "Muntianov", "birthYear": 2007, "group": "user"}
-    logic.add_user(user)
-    logic.delete_user(1)
-    deleted_user = logic.get_user(1)
-    assert deleted_user is None
+    updated_user = {"firstName": "Anna", "lastName": "Muntianova", "birthYear": 2007, "group": "user"}
+    client.post('/users', json=user)
+    client.patch('/users/1', json=updated_user)
+    response = client.get('/users/1')
+    assert response.status_code == 200
+    assert response.get_json() == {"id": 1, "firstName": "Anna", "lastName": "Muntianova", "birthYear": 2007, "group": "user"}
+
+def test_integration_delete_user(client):
+    user = {"firstName": "Eduard", "lastName": "Muntianov", "birthYear": 2007, "group": "user"}
+    client.post('/users', json=user)
+    response = client.delete('/users/1')
+    assert response.status_code == 200
+    response = client.get('/users/1')
+    assert response.status_code == 404
